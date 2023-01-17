8acd9651ddfd9f4a38d4845d191c355b5a9d3ef6
Rohan Dhadge <rohandhadge14@gmail.com>
2023-01-17 12:32:49 +0530

    Initial Commit

---
+++ WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Rohan Dhadge on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
